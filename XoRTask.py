f8ade45837547b851adf53978f7d82ef5cc7d1fb
basra4ka <evgeha_ivanov_01@bk.ru>
2023-11-09 19:03:50 +0300

    commit

--- XoRTask.py
+++ XoRTask.py
@@ -73,57 +73,67 @@ class Neuron_AND():
         return 1 / (1 + torch.exp(torch.tensor(-out)))
 
 
-class Neuron_XOR():
+class Neuron_OR():
     def __init__(self):
         self.weights = list()
         self.b = 1
         for i in range(3):
             self.weights.append(random.random())
 
-    def learn(self, data_train_in, data_train_out, neuron_not, neuron_and):
+    def learn(self, data_train_in, data_train_out):
         i = 0
         for x1, x2 in data_train_in:
-            x = neuron_and.calc({'x': neuron_not.calc(x1).item(), 'y': x2}).item()
-            y = neuron_and.calc({'x': x1, 'y': neuron_not.calc(x2).item()}).item()
-            temp = x * self.weights[0] + y * self.weights[1] + self.b * self.weights[2]
+            temp = x1 * self.weights[0] + x2 * self.weights[1] + self.b * self.weights[2]
             temp = 1 / (1 + torch.exp(torch.tensor(-temp)))
             error = data_train_out[i] - temp
             i += 1
-            self.weights[0] += error * x
-            self.weights[1] += error * y
+            self.weights[0] += error * x1
+            self.weights[1] += error * x2
             self.weights[2] += error * self.b
 
-    def calc(self, input_data, neuron_not, neuron_and):
-        x = neuron_and.calc({'x': neuron_not.calc(input_data['x']).item(), 'y': input_data['y']}).item()
-        y = neuron_and.calc({'x': input_data['x'], 'y': neuron_not.calc(input_data['y']).item()}).item()
-        print('in xor', 'not', input_data['x'], 'and', input_data['y'], '=', x)
-        print('in xor', input_data['x'], 'and not', input_data['y'], '=', y)
-        out = x * self.weights[0] + y * self.weights[1] + self.b * self.weights[2]
+    def calc(self, input_data):
+        out = input_data['x'] * self.weights[0] + input_data['y'] * self.weights[1] + self.b * self.weights[2]
         return 1 / (1 + torch.exp(torch.tensor(-out)))
 
 
 if __name__ == '__main__':
+    # нейрон НЕ
+
+    # данные для обучения
     data_train_not = [
         {"in": [1], "out": [0]},
         {"in": [0], "out": [1]}
     ]
 
-    tensor_train_x = list(map(lambda item: item["in"], data_train_not))
-    tensor_train_y = list(map(lambda item: item["out"], data_train_not))
+    tensor_train_x_not = list(map(lambda item: item["in"], data_train_not))
+    tensor_train_y_not = list(map(lambda item: item["out"], data_train_not))
 
-    tensor_train_x = torch.tensor(tensor_train_x).to(torch.float32).to(tensor_device)
-    tensor_train_y = torch.tensor(tensor_train_y).to(torch.float32).to(tensor_device)
+    tensor_train_x_not = torch.tensor(tensor_train_x_not).to(torch.float32).to(tensor_device)
+    tensor_train_y_not = torch.tensor(tensor_train_y_not).to(torch.float32).to(tensor_device)
 
     neuron_not = Neuron_NOT()
 
+    # обучение
     for i in range(5000):
-        neuron_not.learn(tensor_train_x, tensor_train_y)
+        neuron_not.learn(tensor_train_x_not, tensor_train_y_not)
 
+    # результат
     print("НЕ", 1, "=", round(neuron_not.calc(1).item()))
     print("НЕ", 0, "=", round(neuron_not.calc(0).item()))
 
     print("==============================================")
 
+    # входные значения для нейрона
+    input_data = [
+        {"x": 0, "y": 0},
+        {"x": 0, "y": 1},
+        {"x": 1, "y": 0},
+        {"x": 1, "y": 1}
+    ]
+
+    # нейрон И
+
+    # данные для обучения
     data_train_and = [
         {"in": [0, 0], "out": [0]},
         {"in": [0, 1], "out": [0]},
@@ -131,51 +141,50 @@ if __name__ == '__main__':
         {"in": [1, 1], "out": [1]}
     ]
 
-    train_and_x = list(map(lambda item: item["in"], data_train_and))
-    train_and_y = list(map(lambda item: item["out"], data_train_and))
+    tensor_train_x_and = list(map(lambda item: item["in"], data_train_and))
+    tensor_train_y_and = list(map(lambda item: item["out"], data_train_and))
 
-    train_and_x = torch.tensor(train_and_x).to(torch.float32).to(tensor_device)
-    train_and_y = torch.tensor(train_and_y).to(torch.float32).to(tensor_device)
+    tensor_train_x_and = torch.tensor(tensor_train_x_and).to(torch.float32).to(tensor_device)
+    tensor_train_y_and = torch.tensor(tensor_train_y_and).to(torch.float32).to(tensor_device)
 
     neuron_and = Neuron_AND()
 
+    # обучение
     for i in range(5000):
-        neuron_and.learn(train_and_x, train_and_y)
+        neuron_and.learn(tensor_train_x_and, tensor_train_y_and)
 
-    input_data_and = [
-        {"x": 0, "y": 0},
-        {"x": 0, "y": 1},
-        {"x": 1, "y": 0},
-        {"x": 1, "y": 1}
-    ]
-
-    for data in input_data_and:
+    # результат
+    for data in input_data:
         print(data["x"], "И", data["y"], "=", round(neuron_and.calc(data).item()))
 
-    data_train_xor = [
+    # нейрон ИЛИ
+
+    # данные для обучения
+    data_train_or = [
         {"in": [0, 0], "out": [0]},
         {"in": [0, 1], "out": [1]},
         {"in": [1, 0], "out": [1]},
-        {"in": [1, 1], "out": [0]}
+        {"in": [1, 1], "out": [1]}
     ]
 
-    train_xor_x = list(map(lambda item: item["in"], data_train_and))
-    train_xor_y = list(map(lambda item: item["out"], data_train_and))
+    tensor_train_x_or = list(map(lambda item: item["in"], data_train_or))
+    tensor_train_y_or = list(map(lambda item: item["out"], data_train_or))
 
-    train_xor_x = torch.tensor(train_xor_x).to(torch.float32).to(tensor_device)
-    train_xor_y = torch.tensor(train_xor_y).to(torch.float32).to(tensor_device)
+    tensor_train_x_or = torch.tensor(tensor_train_x_or).to(torch.float32).to(tensor_device)
+    tensor_train_y_or = torch.tensor(tensor_train_y_or).to(torch.float32).to(tensor_device)
 
-    neuron_xor = Neuron_XOR()
+    neuron_or = Neuron_OR()
 
+    # обучение
     for i in range(5000):
-        neuron_xor.learn(train_xor_x, train_xor_y, neuron_not, neuron_and)
+        neuron_or.learn(tensor_train_x_or, tensor_train_y_or)
 
-    input_data_xor = [
-        {"x": 0, "y": 0},
-        {"x": 0, "y": 1},
-        {"x": 1, "y": 0},
-        {"x": 1, "y": 1}
-    ]
+    # результат
+    for data in input_data:
+        print(data["x"], "ИЛИ", data["y"], "=", round(neuron_or.calc(data).item()))
 
-    for data in input_data_xor:
-        print(data['x'], "XOR", data['y'], "=", neuron_xor.calc(data, neuron_not, neuron_and).item())
+    #результат операции XOR
+    for data in input_data:
+        print(data["x"], "XOR", data["y"], "=", neuron_or.calc(
+            {'x': neuron_and.calc({'x': neuron_not.calc(data['x']).item(), 'y': data['y']}).item(),
+             'y': neuron_and.calc({'x': data['x'], 'y': neuron_not.calc(data['y']).item()}).item()}).item())
